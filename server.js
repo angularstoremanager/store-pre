941f69e75a8f3e316ec87a2961e5736cf2281e1a
josedaniel93 <josedanielmartilabrador@gmail.com>
2023-11-26 13:35:15 +0100

    chore: heroku deploy

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + '/dist/store'))
+app.use(express.static(__dirname + '/dist/store/browser'))
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname, '/dist/store', ''))
+  res.sendFile(path.join(__dirname, '/dist/store/browser', ''))
 });
 
 app.listen(process.env.PORT || 5000)
