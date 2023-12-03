bb4258054e1562a935bdf3c3329237f21c335a41
josedaniel93 <josedanielmartilabrador@gmail.com>
2023-12-03 13:17:01 +0100

    feat: navbar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,22 +2,26 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputTextModule } from 'primeng/inputtext';
 import { CarouselModule } from 'primeng/carousel';
+import { MenubarModule } from 'primeng/menubar';
 import { ProductService } from './services/product.service';
 import { Product } from './models/products.interface';
 import { TagModule } from 'primeng/tag';
 import { ButtonModule } from 'primeng/button';
+import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, InputTextModule, CarouselModule, TagModule, ButtonModule],
+  imports: [CommonModule, InputTextModule, CarouselModule, TagModule, ButtonModule, MenubarModule],
   providers: [ProductService],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  value: string | undefined;
+  items: MenuItem[] | undefined;
   products: Product[];
-
   responsiveOptions: any[] | undefined;
+  value: string | undefined;
+
+
 
   constructor(private productService: ProductService) {
     this.value = '';
@@ -46,6 +50,129 @@ export class HomeComponent {
               numScroll: 1
           }
       ];
+      this.items = [
+        {
+            label: 'File',
+            icon: 'pi pi-fw pi-file',
+            items: [
+                {
+                    label: 'New',
+                    icon: 'pi pi-fw pi-plus',
+                    items: [
+                        {
+                            label: 'Bookmark',
+                            icon: 'pi pi-fw pi-bookmark'
+                        },
+                        {
+                            label: 'Video',
+                            icon: 'pi pi-fw pi-video'
+                        }
+                    ]
+                },
+                {
+                    label: 'Delete',
+                    icon: 'pi pi-fw pi-trash'
+                },
+                {
+                    separator: true
+                },
+                {
+                    label: 'Export',
+                    icon: 'pi pi-fw pi-external-link'
+                }
+            ]
+        },
+        {
+            label: 'Edit',
+            icon: 'pi pi-fw pi-pencil',
+            items: [
+                {
+                    label: 'Left',
+                    icon: 'pi pi-fw pi-align-left'
+                },
+                {
+                    label: 'Right',
+                    icon: 'pi pi-fw pi-align-right'
+                },
+                {
+                    label: 'Center',
+                    icon: 'pi pi-fw pi-align-center'
+                },
+                {
+                    label: 'Justify',
+                    icon: 'pi pi-fw pi-align-justify'
+                }
+            ]
+        },
+        {
+            label: 'Users',
+            icon: 'pi pi-fw pi-user',
+            items: [
+                {
+                    label: 'New',
+                    icon: 'pi pi-fw pi-user-plus'
+                },
+                {
+                    label: 'Delete',
+                    icon: 'pi pi-fw pi-user-minus'
+                },
+                {
+                    label: 'Search',
+                    icon: 'pi pi-fw pi-users',
+                    items: [
+                        {
+                            label: 'Filter',
+                            icon: 'pi pi-fw pi-filter',
+                            items: [
+                                {
+                                    label: 'Print',
+                                    icon: 'pi pi-fw pi-print'
+                                }
+                            ]
+                        },
+                        {
+                            icon: 'pi pi-fw pi-bars',
+                            label: 'List'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Events',
+            icon: 'pi pi-fw pi-calendar',
+            items: [
+                {
+                    label: 'Edit',
+                    icon: 'pi pi-fw pi-pencil',
+                    items: [
+                        {
+                            label: 'Save',
+                            icon: 'pi pi-fw pi-calendar-plus'
+                        },
+                        {
+                            label: 'Delete',
+                            icon: 'pi pi-fw pi-calendar-minus'
+                        }
+                    ]
+                },
+                {
+                    label: 'Archieve',
+                    icon: 'pi pi-fw pi-calendar-times',
+                    items: [
+                        {
+                            label: 'Remove',
+                            icon: 'pi pi-fw pi-calendar-minus'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Quit',
+            icon: 'pi pi-fw pi-power-off'
+        }
+    ];
   }
 
   getSeverity(status: string): string | undefined {
